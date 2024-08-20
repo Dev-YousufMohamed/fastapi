d0a24db578b3dc75bf79110cf1aba8a6071ea098
Dev-YousufMohamed <141830411+Dev-YousufMohamed@users.noreply.github.com>
2024-08-20 12:34:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from typing import Union
 from fastapi import FastAPI
-from pytubefix import YouTube
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    youtubeObject = YouTube("https:/t/www.youtube.com/watch?v=9Ipl9BP6rH4")    
-    return {"Hello": youtubeObject.streams.get_highest_resolution().url}
+    return {"Hello": "hi"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
