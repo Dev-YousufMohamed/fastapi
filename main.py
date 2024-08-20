38f74fb475156252d46054cddd925fc673ed25b7
Dev-YousufMohamed <141830411+Dev-YousufMohamed@users.noreply.github.com>
2024-08-20 13:35:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+url = "https://www.youtube.com/watch?v=9Ipl9BP6rH4"
+
 @app.get("/")
 def read_root():
-    return {"Hello": "hi"}
+    return {"Hello": url}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
